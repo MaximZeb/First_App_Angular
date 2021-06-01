ef68e6a7e4c8be503e314a2bb76d474165c06479
Maxim Zebzeev <maksimzeb@yandex.ru>
2021-06-01 12:28:38 +0500

    added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
     styleUrls: ['./app.component.css']
 })
 export class AppComponent { 
-    name = '';
-    isRed = false;
-    color = true;
+    name: string="Tom";
+    age:number = 24;
+    
 }
